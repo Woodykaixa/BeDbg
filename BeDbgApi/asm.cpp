fcbbb96ecfead01904f04328c4d02ae918a47d08
kaixa <690750353@qq.com>
2022-03-04 22:56:14 +0800

    feat: rearrange dep structure

--- BeDbgApi/asm.cpp
+++ BeDbgApi/asm.cpp
@@ -1,9 +1,9 @@
 #include "asm.h"
-#include <Zydis/Decoder.h>
+#include <fmt/core.h>
 
 using namespace BeDbgApi;
 
 Asm::Decoder::Decoder(ZydisMachineMode machineMode, ZydisAddressWidth addressWidth)
 {
-    const auto status = ZydisDecoderInit(&_decoder, machineMode, addressWidth);
+    fmt::print("{} {}\n", machineMode, addressWidth);
 }
